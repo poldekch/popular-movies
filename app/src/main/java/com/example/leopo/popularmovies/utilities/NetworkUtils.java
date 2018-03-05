b805d26e43f20dd6503e9f9f514f4e5ac144e49f
Leopold <leopold.chmielewski@gmail.com>
2018-03-05 22:12:31 +0000

    Added NetworkUtils

---
+++ app/src/main/java/com/example/leopo/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,53 @@
+package com.example.leopo.popularmovies.utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    final static String ORDER_POPULAR = "popular";
+    final static String ORDER_TOP_RATED = "top_rated";
+
+    final static String PARAM_KEY = "api_key";
+    final static String API_KEY = "";
+
+    final static String MOVIEDB_BASE_URL= "http://api.themoviedb.org/3/movie/";
+
+    public static URL buildUrl(String order) {
+        Uri buildUri = Uri.parse(MOVIEDB_BASE_URL + order).buildUpon()
+                .appendQueryParameter(PARAM_KEY, API_KEY).build();
+
+        URL url = null;
+        try {
+            url = new URL(buildUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getApiResponse(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
