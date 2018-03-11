bdd00a4c1fdc4be6474f990c53ff014cdb25d05f
Leopold <leopold.chmielewski@gmail.com>
2018-03-11 19:38:19 +0000

    Added async task

--- app/src/main/java/com/example/leopo/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/leopo/popularmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.leopo.popularmovies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadMovieData();
+    }
+
+    private void loadMovieData() {
+        new FetchMoviesTask().execute();
+    }
+
+    public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(String... strings) {
+            // TODO implement
+            return new String[0];
+        }
+
     }
 }
