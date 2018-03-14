4ad0da2badbec7f80a9ed853b6b5d355ba79db8d
Leopold <leopold.chmielewski@gmail.com>
2018-03-14 21:39:43 +0000

    AsyncTask

--- app/src/main/java/com/example/leopo/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/leopo/popularmovies/MainActivity.java
@@ -4,6 +4,11 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.leopo.popularmovies.utilities.MovieJsonUtils;
+import com.example.leopo.popularmovies.utilities.NetworkUtils;
+
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -21,10 +26,27 @@ public class MainActivity extends AppCompatActivity {
     public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
 
         @Override
-        protected String[] doInBackground(String... strings) {
+        protected String[] doInBackground(String... params) {
             // TODO implement
-            return new String[0];
-        }
+            if (params.length == 0) {
+                return null;
+            }
 
+            String type = params[0];
+
+            // TODO should be order line below
+            URL moviesUrl = NetworkUtils.buildUrl(type);
+
+            try {
+                String movieResponse = NetworkUtils.getApiResponse(moviesUrl);
+
+                String[] simpleJsonMovieData = MovieJsonUtils.getSimpleMovieStringsFromJson(MainActivity.this, movieResponse);
+
+                return simpleJsonMovieData;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/leopo/popularmovies/utilities/MovieJsonUtils.java
@@ -0,0 +1,18 @@
+package com.example.leopo.popularmovies.utilities;
+
+import android.content.Context;
+
+import org.json.JSONException;
+
+public class MovieJsonUtils {
+
+    public static String[] getSimpleMovieStringsFromJson(Context context, String movieJsonString)
+        throws JSONException {
+
+        // TODO here
+
+        String[] parsedMovieData = null;
+
+        return parsedMovieData;
+    }
+}
