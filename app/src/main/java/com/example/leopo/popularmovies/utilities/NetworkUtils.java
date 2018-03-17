a95ce4e0e388bbeb3cafaf5c5c61ace111081cd1
Leopold <leopold.chmielewski@gmail.com>
2018-03-17 14:10:01 +0000

    Image URL

--- app/src/main/java/com/example/leopo/popularmovies/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/leopo/popularmovies/utilities/NetworkUtils.java
@@ -17,7 +17,10 @@ public class NetworkUtils {
     final static String PARAM_KEY = "api_key";
     final static String API_KEY = "";
 
-    final static String MOVIEDB_BASE_URL= "http://api.themoviedb.org/3/movie/";
+    final static String MOVIEDB_BASE_URL = "http://api.themoviedb.org/3/movie/";
+
+    final static String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+    final static String IMAGE_SIZE = "w185";
 
     public static URL buildUrl(String order) {
         Uri buildUri = Uri.parse(MOVIEDB_BASE_URL + order).buildUpon()
@@ -49,5 +52,19 @@ public class NetworkUtils {
         } finally {
             urlConnection.disconnect();
         }
+    }
+
+    public static URL buildImageUrl() {
+        Uri buildUri = Uri.parse(IMAGE_BASE_URL + IMAGE_SIZE).buildUpon()
+                .appendQueryParameter(PARAM_KEY, API_KEY).build();
+
+        URL url = null;
+        try {
+            url = new URL(buildUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
     }
 }
