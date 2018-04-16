ff790cde998f6ff4e870f728aa48f660bf6bcf4d
Leopold <leopold.chmielewski@gmail.com>
2018-04-16 19:49:38 +0100

    Added Movie Contract and DBHelper

---
+++ app/src/main/java/com/example/leopo/popularmovies/data/MovieContract.java
@@ -0,0 +1,16 @@
+package com.example.leopo.popularmovies.data;
+
+import android.provider.BaseColumns;
+
+public class MovieContract {
+
+    public static final class MovieEntry implements BaseColumns {
+        public static final String TABLE_NAME = "movies";
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        public static final String COLUMN_POSTER_URL = "poster_url";
+        public static final String COLUMN_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_PLOT_SYNOPSIS = "plot_synopsis";
+    }
+}
---
+++ app/src/main/java/com/example/leopo/popularmovies/data/MovieDbHelper.java
@@ -0,0 +1,36 @@
+package com.example.leopo.popularmovies.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.leopo.popularmovies.data.MovieContract.MovieEntry;
+
+public class MovieDbHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "movies.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        final String SQL_CREATE_MOVIES_TABLE =
+                "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+                        MovieEntry._ID                  + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        MovieEntry.COLUMN_MOVIE_ID      + " INTEGER, " +
+                        MovieEntry.COLUMN_TITLE         + " TEXT, " +
+                        MovieEntry.COLUMN_RELEASE_DATE  + " TEXT, " +
+                        MovieEntry.COLUMN_POSTER_URL    + " TEXT, " +
+                        MovieEntry.COLUMN_VOTE_AVERAGE  + " REAL, " +
+                        MovieEntry.COLUMN_PLOT_SYNOPSIS + " TEXT " + ");";
+
+        db.execSQL(SQL_CREATE_MOVIES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
