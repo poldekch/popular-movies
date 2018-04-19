b89a5e8caafe7518478678dd0297b2f6c1f6fcdc
Leopold <leopold@motorcheck.ie>
2018-04-19 17:12:55 +0100

    Preparing for DataProvider

--- app/src/main/java/com/example/leopo/popularmovies/data/MovieContentProvider.java
+++ app/src/main/java/com/example/leopo/popularmovies/data/MovieContentProvider.java
@@ -1,7 +1,29 @@
 package com.example.leopo.popularmovies.data;
 
 import android.content.ContentProvider;
+import android.content.Context;
+import android.content.UriMatcher;
 
 public class MovieContentProvider extends ContentProvider {
 
+    public static final int MOVIES = 100;
+    public static final int MOVIES_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES, MOVIES);
+        uriMatcher.addURI(MovieContract.AUTHORITY, MovieContract.PATH_MOVIES + "/#", MOVIES_WITH_ID);
+        return uriMatcher;
+    }
+
+    private MovieDbHelper mMovieDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mMovieDbHelper = new MovieDbHelper(context);
+        return true;
+    }
 }
--- app/src/main/java/com/example/leopo/popularmovies/data/MovieContract.java
+++ app/src/main/java/com/example/leopo/popularmovies/data/MovieContract.java
@@ -1,12 +1,17 @@
 package com.example.leopo.popularmovies.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class MovieContract {
 
-    public static final class MovieEntry implements BaseColumns {
-        public final static String CONTENT_URI = "com.example.leopold.popularmovies";
+    public static final String AUTHORITY = "com.example.android.movies";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    public static final String PATH_MOVIES = "movies";
+    public static final Uri CONTENT_URI =
+            BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
 
+    public static final class MovieEntry implements BaseColumns {
         public static final String TABLE_NAME = "movies";
         public static final String COLUMN_MOVIE_ID = "movie_id";
         public static final String COLUMN_TITLE = "title";
